ce616c5cec556b22fe438b864923f714f245683c
pkfranz <paulkfranz@gmail.com>
2024-01-24 13:36:46 -0500

    afghan line chart toggle

---
+++ afghan-incident-chart/script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    document.getElementById('image1').classList.toggle('show');
+    document.getElementById('image2').classList.toggle('show');
+});
