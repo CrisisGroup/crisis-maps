54530671fdfdb45e1eb896d3d9a73ecd0f997bc1
pkfranz <paulkfranz@gmail.com>
2024-01-29 12:14:29 -0500

    image click toggle

--- afghan-incident-chart/script.js
+++ afghan-incident-chart/script.js
@@ -1,4 +1,8 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
+function toggleImages() {
     document.getElementById('image1').classList.toggle('show');
     document.getElementById('image2').classList.toggle('show');
-});
+}
+
+document.getElementById('toggleButton').addEventListener('click', toggleImages);
+document.getElementById('image1').addEventListener('click', toggleImages);
+document.getElementById('image2').addEventListener('click', toggleImages);
